045317997d576447543fbe3116a48ea89217fd5c
prasad <prasad.kamble@wdtablesystems.com>
2025-09-01 10:31:00 +0530

    Logs function add

--- TestCases/.DummyTestCases/test_TEST-1999.py
+++ TestCases/.DummyTestCases/test_TEST-1999.py
@@ -5,7 +5,7 @@ from ExecutionTemplates.PPExecutionTemplate import PPExecutionTemplate
 @allure.feature("Buy-In Feature")
 @allure.story("test_DummyTestCase2: Rated Buy-In")
 @allure.title("test_DummyTestCase2 Rated Buy-In Test")
-def test_TEST_1999(setup,request):
+def test_TEST_1999(setup):
     ppb = PPExecutionTemplate(setup, "TEST-1999","PlayWright_POC")
-    request.node.tbd = ppb
+    ppb.configuration_login.navigate_to_configuration("Configuration")
     ppb.configuration_actions.drill_down(setup, ppb.config["tableIP"])
